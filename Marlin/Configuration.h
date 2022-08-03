0875cf95329f9a8a9924147813dec44974e9e3df
Christian Coello Grassi <christian.coello@barbri.com>
2022-08-03 15:05:01 -0300

    changed default axis steps per unit for extruders

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1136,7 +1136,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.3, 80.3, 400, 395 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.3, 80.3, 400, 410.2 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
